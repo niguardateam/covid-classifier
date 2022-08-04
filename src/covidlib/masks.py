d1ddbdf2f28d7c65b386c03872f542e3dc48cdea
Andrea Sala <andrea.sala98@gmail.com>
2022-08-04 15:39:35 +0200

    Add tqdm bar for masks

--- src/covidlib/masks.py
+++ src/covidlib/masks.py
@@ -7,8 +7,9 @@ from glob import glob
 import logging
 import os
 import numpy as np
-import SimpleITK as sitk
 from lungmask import mask
+from tqdm import tqdm
+import SimpleITK as sitk
 
 logger = logging.getLogger()
 logger.setLevel(logging.CRITICAL)
@@ -36,10 +37,11 @@ class MaskCreator:
     def run(self):
         """Execute main method of MaskCreator class."""
 
-        print("Creating masks....")
         model = mask.get_model('unet', 'R231CovidWeb')
 
-        for pre_path, isoct_path in zip(self.pre_paths, self.nii_paths):
+        for pre_path, isoct_path in tqdm(
+            zip(self.pre_paths, self.nii_paths), total=len(self.pre_paths), colour='MAGENTA',
+            desc="Creating masks"):
             image = sitk.ReadImage(isoct_path)
             segm = mask.apply(image, model)
             segm *= 10
@@ -49,4 +51,3 @@ class MaskCreator:
             result_out_one = sitk.GetImageFromArray(segm_one)
             sitk.WriteImage(result_out_one, os.path.join(pre_path, self.maskname + "_bilat.nii"))
 
-            print(f'{os.path.join(pre_path, self.maskname)}' + '.nii file created')
